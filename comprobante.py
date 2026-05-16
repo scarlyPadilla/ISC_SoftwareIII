1951ad5226eb34690c89b847fa91a560488a8487
scarly_padilla <scarly.padilla@unisa.edu.mx>
2026-05-16 14:40:23 -0600

    comprobante de pago

---
+++ comprobante.py
@@ -0,0 +1,18 @@
+print ("---------------------------")
+print ("---------------------------")
+
+print ("Comprobante de pago")
+
+print ("----------------------------")
+
+print ("cliente: Scarly Padilla")
+print ("servicio: Sistema de venta")
+print ("plan: Mensual")
+
+print ("-----------------------------")
+
+print ("Subtotal : $149.00")
+print ("IVA 16% : $23.84")
+print ("TOTAL : $172.84")
+ 
+print ("----------------------------")
